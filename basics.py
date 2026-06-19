916b5f1760a7f29b23d1e799aac4a836a4525564
shahidhussaincse07-wq <shahidhussain.cse07@gmail.com>
2026-06-19 12:41:44 +0530

    added cgpa

--- basics.py
+++ basics.py
@@ -1,6 +1,8 @@
 name = "Shahid"
 age = 18
 college = "Chaitanya Bharathi Institute Of Technology" 
+cgpa = 9.7
 print(name)
 print("The age of", name, "is", age)
 print("The college name is", college)
+print("The CGPA of", name, "is", cgpa)
